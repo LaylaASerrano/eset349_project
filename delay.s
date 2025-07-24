0c45b8038027ae50bd6c58adb2a6d4ac0a443094
tharunnayar <tnayar7928@icloud.com>
2025-07-23 21:44:10 -0500

    should be our  final version just need to update loop counts depending on act clock

--- delay.s
+++ delay.s
@@ -15,44 +15,55 @@
 ;     - Shorter delay loop for fine-tuning
 
 ; -------------------------------
-; [Tharun Delay 1.0]
-
-     AREA |.text|, CODE, READONLY
+; [Tharun Delay 1.2]
+; -------------------------------
+    AREA |.text|, CODE, READONLY
 
 ; delay_init
-  
-    EXPORT delay_init
+    EXPORT  delay_init
 delay_init
-    BX LR
+     BX LR
 
-; delay_ms [R0 = # of ms]
 
+; delay_ms [register 0 is # of ms]
     EXPORT delay_ms
-delay_ms    
-
-
-
-
-     EXPORT delay_ms
-
 delay_ms
-     PUSH {R4, LR}          ; Save R4 and LR on stack
-     MOV R4, R0            ; Move delay count to R4 
-        MOV R0, #0            ; Clear R0 for loop counter
-delay_loop_ms
-     CMP R0, R4            ; Compare loop counter with delay count
-     BGE end_delay_ms       ; If loop counter >= delay count, exit loop
-     NOP                    ; No operation (can be replaced with actual delay logic)
-     ADD R0, R0, #1         ; Increment loop counter
-     B delay_loop_ms        ; Repeat the loop
-end_delay_ms
-        POP {R4, PC}          ; Restore R4 and return from function
 
-    EXPORT delay_init
-delay_init
-     BX LR 
-        
+ms_outer_loop
+    CMP r0, #0
+    BEQ ms_done                              ; if r0 == 0 exit loop
+   
+    MOV r1, #8000                            ; need to adjust this when we flash and test
 
+ms_inner_loop
+    SUBS r1, r1, #1                          ; decrement r1 by 1
+    BNE ms_inner_loop                        ; if != 0 
+   
+    SUBS r0, r0 , #1                         ; decrement r0 by 1
+    B ms_outer_loop                          ; repeat outer loop again
 
+ms_done
+    BX LR                                    ; return from function
 
 
+
+; delay_us [optional we probably won't need for such finetuning?]
+
+    EXPORT delay_us
+delay_us
+
+us_outer_loop
+    CMP r0, #0
+    BEQ us_done                             ; if r0 == 0 i.e. if done exit
+   
+    MOV r1, #8                              ; we need to adjust this up or down depending on the actual clock
+
+us_inner_loop
+    SUBS r1, r1, #1                         ; decrement r1 by 1
+    BNE us_inner_loop                       ; loop until r1 == 0
+
+    SUBS r0, r0, #1                         ; decrement r0 by 1
+    B us_outer_loop                         ; repeat outer again
+
+us_done
+    BX LR                                   ; return from function
