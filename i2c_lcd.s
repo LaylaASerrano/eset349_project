64142efd3a87e5834a0f4f297ce11102a2ad8f53
tharunnayar <tnayar7928@icloud.com>
2025-07-24 20:27:14 -0500

    i2c lcd v2 (#9)
    * Implemented i2c_init & helper function
    
    * Implemented i2c_init & helper function 1
    
    * updated implentation of sending data functions with overall code, must finish render function
    
    * should be final of what we need for the lcd including the actual display render

--- i2c_lcd.s
+++ i2c_lcd.s
@@ -18,3 +18,240 @@
 ; TODO: lcd_render:
 ;     - Convert game state (paddles, ball) into LCD characters
 ;     - Call lcd_send_data repeatedly to write to LCD
+
+; -------------------------------
+
+; Int statements 
+AREA i2c_lcd, CODE, READONLY
+        EXPORT i2c_init
+        EXPORT lcd_send_cmd
+        EXPORT lcd_send_data
+        EXPORT lcd_render
+
+        IMPORT paddleLeftY
+        IMPORT paddleRightY
+        IMPORT ballX
+        IMPORT ballY
+LCD_ADDR EQU 0x27                           ; Need accurate i2c address of backpack
+
+; -------------------------------
+; i2c_init: enable gpiob | enable i2c1 | configure pb8/pb9 
+; -------------------------------
+
+    ; enable clocks
+    
+    ; GPIOB clock
+    LDR r0, = 0x40023830                ; RCC_AHB1ENR
+    LDR r1, [r0]
+    ORR r1, r1, #(1 << 1)               ; set bit to enable gpiob
+    STR r1, [r0]
+
+    ; i2c1  clock
+    LDR r0, =40023840                   ; RCC_APB1ENR
+    LDR r1, [r0]
+    ORR r1, r1, #(1 << 21)              ; set bit to enable i2c1
+    STR r1, [r0]
+
+
+    ; configure PB8 & PB9 as AF4
+    LDR r0, =40020400                       ; base address
+    LDR r1, [r0]
+    BIC r1, r1, #(0xF << 16)                ; clear bits MODER8 and 9
+    ORR r1, r1, #(0xA << 16)                ; set to af mode
+    str r1, [r0]                            ; store back to MODER
+
+    ; Set AFRH for PB8 and PB9 to AF4
+
+    LDR r1, [r0, #0x24]
+    BIC r1, r1, #(0xFF << 0)                ; clear AFRH8/9
+    ORR r1, r1, #(0x44 << 0)                ; AF4 for both
+    STR r1, [r0, #0x24]
+
+    ; Configure I2C1 for standard 100kHz
+
+    LDR r0, =0x40005400                     ; i2c1 base
+    
+    ; CR2: set peripheral clock frequency (assuming 16 MHz)
+    MOV r1, #16
+    STR r1, [r0, #0x04]
+  
+    ; CCR: 100kHz speed
+    MOV r1, #80
+    STR r1, [r0, #0x1C]
+   
+    ; TRISE
+    MOV r1, #17
+    STR r1, [r0, #0x20]
+  
+    ; Enable I2C1
+    LDR r1, [r0]
+    ORR r1, r1, #1
+    STR r1, [r0]
+        
+    BX LR
+    ENDP
+
+
+; -------------------------------
+; helper func
+; -------------------------------
+
+send_i2c_byte                                                
+        PUSH {r1, r2, lr}                          
+        LDR r1, =0x40005400                         ; i2c1 base address
+
+    wait_TXE 
+        LDR r2, [r1, #0x14]                         ; i2c_sr1
+        TST r2, #(1 << 7)                           ; check TXE flag set
+        BEQ wait_TXE                                ; if not sent wait
+        STRB r0, [r1, #0x10]                        ; i2c_dr
+
+    wait_BTF
+        LDR r2, [r1, #0x14]                         ; i2c_sr1
+        TST r2, #(1 << 2)                           ; check BTF flag set
+        BEQ wait_BTF                                ; if not set wait
+        POP {r1, r2, pc}                            ; return from
+
+
+; -------------------------------
+; send comamand
+; -------------------------------
+
+lcd_send_cmd function
+    PUSH {r1, r2, LR}                               ; save registers
+    LDR r1, =0x40005400                             ; load i2c1 base
+
+    ; generate start condition
+    LDR r2, [r1]                          
+    ORR r2, r2, #(1 << 8)                           ; set start bit
+    STR r2, [r1] 
+
+    wait_SB
+    LDR r2, [r1, #0x14]
+    TST r2, #(1 << 0)                               ; check SB flag set
+    BEQ wait_SB                                     ; if not set, wait
+
+    ; send address with write bit
+    MOV r2, #(LCD_ADDR << 1)                        ; shift address left and set write bit
+    STRB r2, [r1, #0x10]
+
+    wait_ADDR
+    LDR r2, [r1, #0x14]
+    TST r2, #(1 << 1)                               ; check ADDR flag set
+    BEQ wait_ADDR                                   ; if not set wait
+    LDR r2, [r1, #0x18]                             ; clear ADDR flag
+
+    ; send command byte
+    BL send_i2c_byte
+
+    ; generate stop condition
+    LDR r2, [r1]                                    ; read i2c1 base
+    ORR r2, r2, #(1 << 9)                           ; set stop bit
+    STR r2, [r1]                                    ; write back 
+    
+    POP {r1, r2, PC}                                ; restore registers and return
+    ENDP
+
+; -------------------------------
+; send data
+; -------------------------------
+lcd_send_data FUNCTION
+    BL lcd_send_cmd
+    BX LR
+    ENDP
+
+; -------------------------------
+; lcd_render: draw paddles and ball based on game state
+; -------------------------------
+lcd_render FUNCTION
+        ; Clear display
+        MOV r0, #0x01
+        BL lcd_send_cmd
+
+        ; Load game state
+        LDR r6, =paddleLeftY
+        LDR r6, [r6]
+        LDR r7, =paddleRightY
+        LDR r7, [r7]
+        LDR r8, =ballX
+        LDR r8, [r8]
+        LDR r9, =ballY
+        LDR r9, [r9]
+
+        ; Row loop
+        MOV r4, #0
+row_loop
+        CMP r4, #4
+        BEQ render_done
+
+        ; Column loop
+        MOV r5, #0
+col_loop
+        CMP r5, #16
+        BEQ next_row
+
+        ; Default char
+        MOV r0, #' '
+
+        ; Left paddle
+        CMP r5, #0
+        BNE check_right
+        CMP r4, r6
+        BNE check_right
+        MOV r0, #'|'
+
+check_right
+        CMP r5, #15
+        BNE check_ball
+        CMP r4, r7
+        BNE check_ball
+        MOV r0, #'|'
+
+check_ball
+        CMP r5, r8
+        BNE send_char
+        CMP r4, r9
+        BNE send_char
+        MOV r0, #'O'
+
+send_char
+        BL lcd_send_data
+        ADD r5, r5, #1
+        B col_loop
+
+next_row
+        ; Move cursor to next row start
+        CMP r4, #0
+        BNE row1done
+        MOV r0, #0xC0
+        BL lcd_send_cmd
+row1done
+        CMP r4, #1
+        BNE row2done
+        MOV r0, #0x94
+        BL lcd_send_cmd
+row2done
+        CMP r4, #2
+        BNE row3done
+        MOV r0, #0xD4
+        BL lcd_send_cmd
+row3done
+        ADD r4, R4, #1
+        B row_loop
+
+render_done
+        BX LR
+        ENDP
+
+END
+
+
+
+
+; Need to define these variables in main.s 
+;       AREA GameData, DATA, READWRITE
+; paddleLeftY   DCD 1
+; paddleRightY  DCD 2
+; ballX         DCD 7
+; ballY         DCD 1
+;    END
